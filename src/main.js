186815860c110b6a807464801bcd8cc27d82b8a6
atoris1192 <atoris1192@gmail.com>
2019-02-19 14:07:31 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+
+      // Initialize Firebase
+      var config = {
+        apiKey: "AIzaSyC6jhwaQlnfRhNnBbW7jAUpA-jTsAw_UOQ",
+        authDomain: "shops-9a76c.firebaseapp.com",
+        databaseURL: "https://shops-9a76c.firebaseio.com",
+        projectId: "shops-9a76c",
+        storageBucket: "shops-9a76c.appspot.com",
+        messagingSenderId: "868347501560"
+      };
+      firebase.initializeApp(config);
